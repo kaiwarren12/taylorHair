d50e127e3386261b85c3c7f8b04b18e1ae4de773
Marco Bejarano <marco.bejarano03@gmail.com>
2021-03-27 19:19:26 -0700

    setting up backend to to hand off to react

--- server.js
+++ server.js
@@ -5,3 +5,19 @@ const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/salonapp"));
+}
+
+app.use(routes);
+
+mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost/salonapp"
+);
+
+app.listen(PORT, function(){
+    console.log(`App is listening on ${PORT}`)
+});
